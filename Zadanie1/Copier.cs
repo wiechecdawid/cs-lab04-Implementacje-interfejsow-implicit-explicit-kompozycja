a4f763934aabce9521172016c074cf114dd3dc36
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-01 00:44:13 +0100

    Zadanie 1

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ver1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Today} Print: {document.GetFileName()}");
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            string sType;
+
+            switch(formatType)
+            {
+                case IDocument.FormatType.TXT:
+                    sType = "Text";
+                    break;
+                case IDocument.FormatType.PDF:
+                    sType = "PDF";
+                    break;
+                default:
+                    sType = "Image";
+                    break;
+            }
+
+            string name = string.Format("{0}Scan{1}.{2}", sType, ScanCounter+1, formatType.ToString().ToLower());
+
+            if (formatType == IDocument.FormatType.PDF)
+                document = new PDFDocument(name);
+            if (formatType == IDocument.FormatType.JPG)
+                document = new ImageDocument(name);
+            else
+                document = new TextDocument(name);
+
+            if (state == IDevice.State.on)
+            {
+                ScanCounter++;
+                Console.WriteLine($"{DateTime.Today} Scan: {document.GetFileName()}");
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            Scan(out IDocument doc);
+            Print(doc);
+        }
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+
+            Console.WriteLine(doc2.GetFileName());
+
+            xerox.PowerOff();
+            xerox.Scan(out doc2);
+            Console.WriteLine(doc2.GetFileName());
+        }
+    }
+}

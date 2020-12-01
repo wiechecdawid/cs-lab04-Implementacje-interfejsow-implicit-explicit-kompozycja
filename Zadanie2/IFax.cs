4ce1fdaaa40929cff6c44be68c43330571c0362c
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-01 23:59:08 +0100

    Zadanie 2

---
+++ Zadanie2/IFax.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ver1;
+
+namespace Zadanie2
+{
+    public interface IFax : IDevice
+    {
+        void Send(IDocument document, string faxNumber);
+    }
+}

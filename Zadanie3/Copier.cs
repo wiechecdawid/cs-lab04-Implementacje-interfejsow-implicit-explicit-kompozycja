6f57a7659760ed716c7368873cb992043d0759df
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-03 21:48:41 +0100

    Zadanie 3

---
+++ Zadanie3/Copier.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie3
+{
+    public class Copier : BaseDevice
+    {
+        private IPrinter _printer;
+        private IScanner _scanner;
+
+        public Copier(IPrinter printer, IScanner scanner)
+        {
+            _printer = printer;
+            _scanner = scanner;
+        }
+
+        public int PrintCounter { get; private set; }
+        public int ScanCounter { get; private set; }
+
+        public void PrinterOn()
+        {
+            this._printer.PowerOn();
+        }
+
+        public void PrinterOff()
+        {
+            this._printer.PowerOff();
+        }
+
+        public void ScannerOn()
+        {
+            this._scanner.PowerOn();
+        }
+
+        public void ScannerOff()
+        {
+            this._scanner.PowerOff();
+        }
+
+        public void Print(in IDocument document)
+        {
+            if(GetState() == IDevice.State.on)
+            {
+                PrinterOn();
+                _printer.Print(in document);
+                PrintCounter++;
+                PrinterOff();
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType type = IDocument.FormatType.JPG)
+        {
+            if (type == IDocument.FormatType.JPG)
+                document = new ImageDocument("ImageScan.jpg");
+            else if (type == IDocument.FormatType.PDF)
+                document = new PDFDocument("PDFScan.jpg");
+            else
+                document = new TextDocument("TextScan/jpg");
+
+            if(GetState() == IDevice.State.on)
+            {
+                ScannerOn();
+                _scanner.Scan(out document, type);
+                ScanCounter++;
+                ScannerOff();
+            }
+        }
+
+        public void ScanAndPrint(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            if (formatType == IDocument.FormatType.JPG)
+                document = new ImageDocument("ImageScan.jpg");
+            else if (formatType == IDocument.FormatType.PDF)
+                document = new PDFDocument("PDFScan.jpg");
+            else
+                document = new TextDocument("TextScan/jpg");
+
+            if (GetState() == IDevice.State.on)
+            {
+                ScannerOn();
+                _scanner.Scan(out document, formatType);
+                ScanCounter++;
+                ScannerOff();
+
+                PrinterOn();
+                _printer.Print(in document);
+                PrintCounter++;
+                PrinterOff();
+            }
+        }
+    }    
+}
